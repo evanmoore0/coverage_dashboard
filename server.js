4d680b6d5829181ae23d05b8e6d82cdd48b69226
evanmoore <evan_moore@brown.edu>
2023-07-17 08:52:12 -0700

    plz

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ const PORT = process.env.PORT || 5001;
 
 // app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 const fs = require('fs');
-const path = require('path');
-
 //Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
