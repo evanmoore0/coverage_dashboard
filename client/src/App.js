e555a1dddcc5bcd975af2574970565ab46b94207
evanmoore <evan_moore@brown.edu>
2023-07-17 09:18:57 -0700

    nice

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,12 @@ function App() {
   const [newsData, setNewsData] = useState(null);
 
   async function getRatings() {
-    const response = await fetch("https://final-dashboard-15ef7bba4916.herokuapp.com/api/ratings");
+
+    const response = await fetch("https://final-dashboard-15ef7bba4916.herokuapp.com/api/ratings").catch((err) => {
+      console.log("ERROR HERE")
+      console.log(err.message)
+      console.log(err.code)
+    });
     const body = await response.json();
 
     if (response.status !== 200) {
