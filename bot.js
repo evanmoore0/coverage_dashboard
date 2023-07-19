4242d4a347ef4545653f5e076c01796dbae2178d
evanmoore <evan_moore@brown.edu>
2023-07-19 08:30:31 -0700

    nice

---
+++ bot.js
@@ -0,0 +1,31 @@
+const puppeteer = require("puppeteer");
+
+const bot = async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--no-sandbox",
+      // "--disable-setuid-sandbox",
+    ],
+  });
+
+  try {
+    const page = await browser.newPage();
+    await page.goto("https://www.glassdoor.com/Reviews/Arista-Networks-Reviews-E295128.htm", {
+        waitUntil: "networkidle2",
+    });
+    
+  } catch (error) {
+
+    console.log("ERROR")
+    return 'Error'
+    
+  } finally {
+    await browser.close();
+  }
+
+  return 'Done'
+};
+
+
+module.exports = bot;
